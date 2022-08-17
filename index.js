e71e18f4ece7a53f01e20cd1616f38b9315945a2
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-17 17:35:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function superbowlWin (arrayOfObjects){
+    const result = arrayOfObjects.find(item =>{
+        if(item.result === "W"){
+            return true;
+        }
+    })
+
+    if(result){
+        return result.year;
+    }
+}
+
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+console.log(superBowlWin(record))
